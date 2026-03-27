68b24ef084a64c5a795dfaccc57ae2dab729d922
alecvolkert <alecvolkert@gmail.com>
2026-03-27 15:48:21 +0000

    to mac

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,  request, jsonify
+from flask import Flask, request, jsonify, render_template
 import psycopg2
 import os
 from dotenv import load_dotenv
@@ -9,7 +9,7 @@ load_dotenv()
 
 DB_CONFIG = {
     "dbname": "environment",
-    "user": "postgres",
+    "user": "alecvolkert",
     "password": os.getenv("DB_PASSWORD"),
     "host": "localhost",
     "port": "5432"
@@ -18,12 +18,12 @@ DB_CONFIG = {
 
 @app.route('/')
 def index():
-	return render_template("~/html/index.html")
+	return render_template('index.html')
 
 
 @app.route('/api/submit', methods = ['POST'])
 def submit_reading():
-	if request.headers.get('API_KEY') != os.getenv("API_KEY"):
+	if request.headers.get('X-API-Key') != os.getenv("API_KEY"):
 		return jsonify({"error":"unauthaurized"}), 401 
 	data = request.get_json()
 	conn = 	psycopg2.connect(**DB_CONFIG)
@@ -36,25 +36,28 @@ def submit_reading():
 	conn.commit()
 	cur.close()
 	conn.close()
+	return jsonify({"status": "ok"}), 200
 
 @app.route('/api/readings')
 def get_readings():
 	conn = psycopg2.connect(**DB_CONFIG)
 	cur = conn.cursor()
-	cur.execute("SELECT timestamp, tempurature, humidity, pressure, oxidising, reducing, nh3, PM2.5  FROM environment")
+	cur.execute("SELECT timestamp, temperature, humidity, pressure, oxidising, reducing, nh3, PM1, PM25, PM10 FROM enviroreadings ORDER BY timestamp DESC LIMIT 500")
 	rows = cur.fetchall()
 	cur.close()
 	conn.close()
 	data = []
 	for row in rows:
 		data.append({
-			"timestamp": row[0],
-			"tempurature": row[1],
+			"timestamp": row[0].isoformat(),
+			"temperature": row[1],
 			"humidity": row[2],
 			"pressure": row[3],
 			"oxidising": row[4],
 			"reducing": row[5],
 			"nh3": row[6],
-			"PM2.5":row[7]
+			"PM1": row[7],
+			"PM25": row[8],
+			"PM10": row[9]
 		})
 	return jsonify(data)
